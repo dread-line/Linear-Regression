8b7a96fbbf96fef7dc7d9e611abdf2f9881f556a
dread-line <lovelycis.25@gmail.com>
2025-10-21 16:56:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-# years of experience > independent variable > x
-# salary > dependent > y
-
-# 1 import libraries
+# 1 import library
 from sklearn.linear_model import LinearRegression
 import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
 
-# 2 siapkan data
-X = [[1], [2], [3], [4]] # independent variable perlu list atau array 2 dimensi krn independent variable bisa 2
-Y = [101, 102, 103, 104] # dependent prlu list atau array 1 dimensi
+# 2 menyiapkan data
+X = [[1], [2], [3], [4]] #tipe data list 2 dimensi
+y = [101, 102, 103, 104] #tipe data list 1 dimensi
 
-# 3 intansiasi objek
+# 3 instansiasi objek
 model = LinearRegression()
+
 st.title("Prediksi Gaji")
 
-# 4 training model / linear reggression
-model.fit(X, Y)
-input_user = st.number_input("Masukkan value: ")
+# 4 training
+model.fit(X, y)
+input_user = st.number_input("Masukan value :")
+
 prediction = model.predict([[input_user]])
 
 # 6 visualisasi
 fig, ax = plt.subplots()
+
 prediksi_y = model.predict(X)
-ax.scatter(X, Y)
+ax.scatter(X, y)
 ax.plot(X, prediksi_y)
 ax.scatter([input_user], [prediction])
+
 st.pyplot(fig)
 st.metric(label="Gaji", value=prediction)
